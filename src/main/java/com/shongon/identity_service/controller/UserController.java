ecdad240ab84ec3745b2b4fa97f415d98261ba60
Hong Son <hongsonn539@gmail.com>
2025-01-06 00:28:54 +0700

    feat: basic CRUD API - day1

---
+++ src/main/java/com/shongon/identity_service/controller/UserController.java
@@ -0,0 +1,43 @@
+package com.shongon.identity_service.controller;
+
+import com.shongon.identity_service.dto.request.CreateUserRequest;
+import com.shongon.identity_service.dto.request.UpdateUserRequest;
+import com.shongon.identity_service.entity.User;
+import com.shongon.identity_service.service.UserService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("/users")
+public class UserController {
+    @Autowired
+    private UserService userService;
+
+    @PostMapping("/register")
+    public User register(@RequestBody CreateUserRequest request) {
+        return userService.createUser(request);
+    }
+
+    @GetMapping("/view-all")
+    public List<User> getAllUsers() {
+        return userService.getAllUsers();
+    }
+
+    @GetMapping("/{userId}")
+    public User getUserById(@PathVariable String userId) {
+        return userService.getUserById(userId);
+    }
+
+    @PutMapping("/update/{userId}")
+    public User updateUser(@PathVariable String userId, @RequestBody UpdateUserRequest request) {
+        return userService.updateUser(userId, request);
+    }
+
+    @DeleteMapping("/delete/{userId}")
+    public String deleteUser(@PathVariable String userId) {
+        userService.deleteUser(userId);
+        return "User has been deleted!";
+    }
+}
---
+++ src/main/java/com/shongon/identity_service/service/UserService.java
@@ -0,0 +1,53 @@
+package com.shongon.identity_service.service;
+
+import com.shongon.identity_service.dto.request.CreateUserRequest;
+import com.shongon.identity_service.dto.request.UpdateUserRequest;
+import com.shongon.identity_service.entity.User;
+import com.shongon.identity_service.repository.UserRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+import java.util.Optional;
+
+@Service
+public class UserService {
+    @Autowired
+    private UserRepository userRepository;
+
+    public User createUser(CreateUserRequest request) {
+        User user = new User();
+
+        user.setUsername(request.getUsername());
+        user.setPassword(request.getPassword());
+        user.setFirstName(request.getFirstName());
+        user.setLastName(request.getLastName());
+        user.setBirthDate(request.getBirthDate());
+
+        return userRepository.save(user);
+    }
+
+    public List<User> getAllUsers() {
+        return userRepository.findAll();
+    }
+
+    public User getUserById(String id) {
+        return userRepository.findById(id)
+                .orElseThrow(() -> new RuntimeException("User not found"));
+    }
+
+    public User updateUser(String userId, UpdateUserRequest request) {
+        User user = getUserById(userId);
+
+        user.setPassword(request.getPassword());
+        user.setFirstName(request.getFirstName());
+        user.setLastName(request.getLastName());
+        user.setBirthDate(request.getBirthDate());
+
+        return userRepository.save(user);
+    }
+
+    public void deleteUser(String userId) {
+        userRepository.deleteById(userId);
+    }
+}
