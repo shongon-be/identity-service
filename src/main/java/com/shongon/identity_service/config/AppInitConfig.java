a5ced8787fee1d519693f61b3136eeec6e0d119a
Hong Son <hongsonn539@gmail.com>
2025-01-16 19:35:51 +0700

    feat: initial Admin account

---
+++ src/main/java/com/shongon/identity_service/config/AppInitConfig.java
@@ -0,0 +1,43 @@
+package com.shongon.identity_service.config;
+
+import com.shongon.identity_service.entity.User;
+import com.shongon.identity_service.enums.Role;
+import com.shongon.identity_service.repository.UserRepository;
+import lombok.AccessLevel;
+import lombok.RequiredArgsConstructor;
+import lombok.experimental.FieldDefaults;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.boot.ApplicationRunner;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.crypto.password.PasswordEncoder;
+
+import java.util.HashSet;
+
+@Slf4j
+@Configuration
+@RequiredArgsConstructor
+@FieldDefaults(level = AccessLevel.PRIVATE, makeFinal = true)
+public class AppInitConfig {
+
+    PasswordEncoder passwordEncoder;
+
+    @Bean
+    ApplicationRunner applicationRunner(UserRepository userRepository) {
+        return args -> {
+            if (userRepository.findByUsername("admin").isEmpty()){
+                var adminRole = new HashSet<String>();
+                adminRole.add(Role.ADMIN.name());
+
+                User admin = User.builder()
+                        .username("admin")
+                        .password(passwordEncoder.encode("admin"))
+                        .roles(adminRole)
+                        .build();
+
+                userRepository.save(admin);
+                log.warn("Admin has been created with default password: admin, please change password!");
+            }
+        };
+    }
+}
